2bdc6227b966842038aecd836751631a0a912494
xuansonkrt <xuansonkrt@gmail.com>
2019-12-22 17:06:38 +0700

    db

---
+++ schema/20191222.sql
@@ -0,0 +1,486 @@
+-- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
+--
+-- Host: localhost    Database: db_tms
+-- ------------------------------------------------------
+-- Server version	8.0.13
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `category` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `code` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `name` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `category_type_id` bigint(20) DEFAULT NULL,
+  `effect_date` date DEFAULT NULL,
+  `expired_date` date DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `created_by` bigint(20) DEFAULT NULL,
+  `modified_date` datetime DEFAULT NULL,
+  `modified_by` bigint(20) DEFAULT NULL,
+  `the_order` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'Giao_Su','Giáo sư',1,'2019-12-02','2019-12-22',NULL,NULL,'2019-12-21 18:27:58',0,1),(2,'PGS','Phó giáo sư',1,'2019-12-02','2020-01-02','2019-12-21 18:22:41',0,NULL,NULL,2),(3,'master','master',1,'2019-12-04','2019-12-17','2019-12-21 18:27:48',0,NULL,NULL,3),(34,'TG','Trợ giảng',7,NULL,NULL,'2019-12-21 23:38:14',0,NULL,NULL,1),(35,'GV','Giảng viên',7,'2019-12-01','2019-12-31','2019-12-21 23:38:31',0,'2019-12-21 23:38:55',0,2),(36,'GVC','Giảng viên chính',7,NULL,NULL,'2019-12-21 23:38:47',0,NULL,NULL,3),(37,'GVCC','Giảng viên cao cấp',7,NULL,NULL,'2019-12-21 23:39:11',0,NULL,NULL,4),(38,'CD','Cao đẳng',8,NULL,NULL,'2019-12-22 13:17:20',0,NULL,NULL,1),(39,'DH','Đại học',8,'2019-12-01','2019-12-25','2019-12-22 13:17:29',0,'2019-12-22 13:17:45',0,2),(40,'SDH','Sau đại học',8,NULL,NULL,'2019-12-22 13:17:38',0,NULL,NULL,3);
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `category_type`
+--
+
+DROP TABLE IF EXISTS `category_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `category_type` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `code` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `name` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `effect_date` date DEFAULT NULL,
+  `expired_date` date DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `created_by` bigint(20) DEFAULT NULL,
+  `modified_date` datetime DEFAULT NULL,
+  `modified_by` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category_type`
+--
+
+LOCK TABLES `category_type` WRITE;
+/*!40000 ALTER TABLE `category_type` DISABLE KEYS */;
+INSERT INTO `category_type` VALUES (1,'HOC_HAM','Học hàm','2019-12-02','2020-01-01',NULL,NULL,'2019-12-20 15:49:39',0),(2,'HOC_VI','Học vị','2019-12-10','2019-12-30',NULL,NULL,'2019-12-20 15:53:33',0),(5,'CHUC_VU_DANG','Chức vụ Đảng','2019-12-02','2019-12-24','2019-12-20 15:54:36',0,NULL,NULL),(6,'CHUC_VU_CHINH_QUYEN','Chức vụ chính quyền','2019-12-17','2019-12-26','2019-12-20 15:55:06',0,NULL,NULL),(7,'CDNG','Chức danh nhà giáo',NULL,NULL,'2019-12-21 23:37:56',0,NULL,NULL),(8,'EDU_LEVEL','Cấp học',NULL,NULL,'2019-12-22 13:16:57',0,NULL,NULL);
+/*!40000 ALTER TABLE `category_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `course`
+--
+
+DROP TABLE IF EXISTS `course`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `course` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `code` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `name` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `term_id` bigint(20) DEFAULT NULL,
+  `subject_id` bigint(20) DEFAULT NULL,
+  `student_amount` int(11) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `created_by` bigint(20) DEFAULT NULL,
+  `modified_date` datetime DEFAULT NULL,
+  `modified_by` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `course`
+--
+
+LOCK TABLES `course` WRITE;
+/*!40000 ALTER TABLE `course` DISABLE KEYS */;
+/*!40000 ALTER TABLE `course` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `learning_type`
+--
+
+DROP TABLE IF EXISTS `learning_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `learning_type` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `code` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `name` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `note` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `education_level_id` bigint(20) DEFAULT NULL,
+  `is_detail` tinyint(1) DEFAULT NULL,
+  `factor` double DEFAULT NULL,
+  `unit` double DEFAULT NULL,
+  `unit_name` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `created_by` bigint(20) DEFAULT NULL,
+  `modified_date` datetime DEFAULT NULL,
+  `modified_by` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `learning_type`
+--
+
+LOCK TABLES `learning_type` WRITE;
+/*!40000 ALTER TABLE `learning_type` DISABLE KEYS */;
+INSERT INTO `learning_type` VALUES (1,'123','123','update   ',39,NULL,1,1,'1','2019-12-22 13:24:23',0,'2019-12-22 13:29:19',0);
+/*!40000 ALTER TABLE `learning_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `organization`
+--
+
+DROP TABLE IF EXISTS `organization`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `organization` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `code` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `name` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `note` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `phone_number` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `address` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `parent_id` bigint(20) DEFAULT NULL,
+  `path` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `level` int(11) DEFAULT NULL,
+  `the_order` int(11) DEFAULT NULL,
+  `founded_date` date DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `created_by` bigint(20) DEFAULT NULL,
+  `modified_date` datetime DEFAULT NULL,
+  `modified_by` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `organization`
+--
+
+LOCK TABLES `organization` WRITE;
+/*!40000 ALTER TABLE `organization` DISABLE KEYS */;
+/*!40000 ALTER TABLE `organization` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `paper_type`
+--
+
+DROP TABLE IF EXISTS `paper_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `paper_type` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `code` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `name` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `note` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `paper_level_id` bigint(20) DEFAULT NULL,
+  `research_point` double DEFAULT NULL,
+  `unit` double DEFAULT NULL,
+  `unit_name` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `created_by` bigint(20) DEFAULT NULL,
+  `modified_date` datetime DEFAULT NULL,
+  `modified_by` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `paper_type`
+--
+
+LOCK TABLES `paper_type` WRITE;
+/*!40000 ALTER TABLE `paper_type` DISABLE KEYS */;
+INSERT INTO `paper_type` VALUES (2,'234','234','      update                      ',NULL,234,234,'234','2019-12-22 16:52:01',0,'2019-12-22 16:53:25',0),(3,'234','23','                            ',NULL,23,23,'23','2019-12-22 16:52:44',0,NULL,NULL),(4,'3ẻ','dfg','                            ',NULL,234,123,'dfg','2019-12-22 16:53:16',0,NULL,NULL);
+/*!40000 ALTER TABLE `paper_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `code` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `name` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `staff`
+--
+
+DROP TABLE IF EXISTS `staff`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `staff` (
+  `id` bigint(20) NOT NULL,
+  `code` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `name` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `gender` tinyint(1) DEFAULT NULL,
+  `date_of_birth` date DEFAULT NULL,
+  `phone_number` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `address` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `user_name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password_hash` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `organization_id` bigint(20) DEFAULT NULL,
+  `army_rank_id` bigint(20) DEFAULT NULL,
+  `academic_level_id` bigint(20) DEFAULT NULL,
+  `academic_title_id` bigint(20) DEFAULT NULL,
+  `level_title_id` bigint(20) DEFAULT NULL,
+  `avatar` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `teaching` tinyint(1) DEFAULT NULL,
+  `manager` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `staff`
+--
+
+LOCK TABLES `staff` WRITE;
+/*!40000 ALTER TABLE `staff` DISABLE KEYS */;
+/*!40000 ALTER TABLE `staff` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `staff_course`
+--
+
+DROP TABLE IF EXISTS `staff_course`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `staff_course` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `staff_id` bigint(20) DEFAULT NULL,
+  `course_id` bigint(20) DEFAULT NULL,
+  `learning_type_id` bigint(20) DEFAULT NULL,
+  `amount` double DEFAULT NULL,
+  `edu_point` double DEFAULT NULL,
+  `organization_id` bigint(20) DEFAULT NULL,
+  `term` int(11) DEFAULT NULL,
+  `year` int(11) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `created_by` bigint(20) DEFAULT NULL,
+  `modified_date` datetime DEFAULT NULL,
+  `modified_by` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `staff_course`
+--
+
+LOCK TABLES `staff_course` WRITE;
+/*!40000 ALTER TABLE `staff_course` DISABLE KEYS */;
+/*!40000 ALTER TABLE `staff_course` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `staff_paper`
+--
+
+DROP TABLE IF EXISTS `staff_paper`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `staff_paper` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `staff_id` bigint(20) DEFAULT NULL,
+  `code` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `name` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `author` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `number_author` int(11) DEFAULT NULL,
+  `publish_date` date DEFAULT NULL,
+  `term` int(11) DEFAULT NULL,
+  `year` int(11) DEFAULT NULL,
+  `organization_id` bigint(20) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `created_by` bigint(20) DEFAULT NULL,
+  `modified_date` datetime DEFAULT NULL,
+  `modified_by` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `staff_paper`
+--
+
+LOCK TABLES `staff_paper` WRITE;
+/*!40000 ALTER TABLE `staff_paper` DISABLE KEYS */;
+/*!40000 ALTER TABLE `staff_paper` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `staff_role`
+--
+
+DROP TABLE IF EXISTS `staff_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `staff_role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `staff_id` bigint(20) DEFAULT NULL,
+  `role_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `staff_role`
+--
+
+LOCK TABLES `staff_role` WRITE;
+/*!40000 ALTER TABLE `staff_role` DISABLE KEYS */;
+/*!40000 ALTER TABLE `staff_role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `subject`
+--
+
+DROP TABLE IF EXISTS `subject`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `subject` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `code` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `name` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `note` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `education_level_id` bigint(20) DEFAULT NULL,
+  `learning_type_id` bigint(20) DEFAULT NULL,
+  `credit` int(11) DEFAULT NULL COMMENT 'số tín chỉ',
+  `class_period` int(11) DEFAULT NULL COMMENT 'số tiết',
+  `organization_id` bigint(20) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `created_by` bigint(20) DEFAULT NULL,
+  `modified_date` datetime DEFAULT NULL,
+  `modified_by` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `subject`
+--
+
+LOCK TABLES `subject` WRITE;
+/*!40000 ALTER TABLE `subject` DISABLE KEYS */;
+/*!40000 ALTER TABLE `subject` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `target`
+--
+
+DROP TABLE IF EXISTS `target`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `target` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `edu_duty` int(11) DEFAULT NULL COMMENT 'định mức giảng dạy\n',
+  `research_duty` int(11) DEFAULT NULL COMMENT 'định mức nghiên cứu',
+  `the_type` bigint(20) DEFAULT NULL,
+  `staff_property_id` bigint(20) DEFAULT NULL COMMENT 'id chức danh',
+  `created_date` datetime DEFAULT NULL,
+  `created_by` bigint(20) DEFAULT NULL,
+  `modified_date` datetime DEFAULT NULL,
+  `modified_by` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `target`
+--
+
+LOCK TABLES `target` WRITE;
+/*!40000 ALTER TABLE `target` DISABLE KEYS */;
+INSERT INTO `target` VALUES (2,23,32,7,37,'2019-12-22 01:00:09',0,NULL,NULL),(4,123,123,7,34,'2019-12-22 01:03:37',0,NULL,NULL),(5,123,123,7,36,'2019-12-22 01:08:36',0,NULL,NULL),(6,343,343,7,35,'2019-12-22 01:08:56',0,NULL,NULL);
+/*!40000 ALTER TABLE `target` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `term`
+--
+
+DROP TABLE IF EXISTS `term`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `term` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `code` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `name` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `year` int(11) DEFAULT NULL,
+  `begin_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL,
+  `the_order` int(11) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `created_by` bigint(20) DEFAULT NULL,
+  `modified_date` datetime DEFAULT NULL,
+  `modified_by` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `term`
+--
+
+LOCK TABLES `term` WRITE;
+/*!40000 ALTER TABLE `term` DISABLE KEYS */;
+INSERT INTO `term` VALUES (3,'HK2','Học kỳ 2 update',2019,'2019-12-01','2019-12-31',1,'2019-12-21 22:57:19',0,'2019-12-21 22:58:23',0);
+/*!40000 ALTER TABLE `term` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'db_tms'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-12-22 17:05:24
